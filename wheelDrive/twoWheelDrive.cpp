156176d7077404e4d84f2cadbd7e4f51d087766c
Woocheck <woocheck@op.pl>
2019-07-13 21:35:14 +0200

    Drive controll class has been simplified.

--- wheelDrive/twoWheelDrive.cpp
+++ wheelDrive/twoWheelDrive.cpp
@@ -1,1 +1,16 @@
+/* 
+* TwoWheelDrive.h
+*
+* Created: 13.07.2019 
+* Author: Woocheck
+*/
 
+#include "./twoWheelDrive.h"
+#include "../pin_settings.h"
+#include "../dcMotor/DCmotor.h"
+
+void TwoWheelDrive::driveControll( int leftSpeed, int rightSpeed )
+{
+	leftDC.control( leftSpeed );
+	rightDC.control( rightSpeed );
+};
--- wheelDrive/twoWheelDrive.h
+++ wheelDrive/twoWheelDrive.h
@@ -1,1 +1,42 @@
+/* 
+* TwoWheelDrive.h
+*
+* Created: 13.07.2019 
+* Author: Woocheck
+*/
+
+
+#ifndef __TWO_WHEEL_DRIVE_CLASS_H__
+#define __TWO_WHEEL_DRIVE_CLASS_H__
+
+
+#include "../pin_settings.h"
+#include "../dcMotor/DCmotor.h"
+
+
+
+class TwoWheelDrive
+{
+	private:               
+		DcMotor leftDC;
+		DcMotor rightDC;
+
+	public:
+		TwoWheelDrive():
+			leftDC(  PIN_LEFT_A, PIN_LEFT_B , PIN_LEFT_ENABLE ),
+			rightDC( PIN_RIGHT_A, PIN_RIGHT_B , PIN_RIGHT_ENABLE )
+		{
+			wiringPiSetup();
+		};
+		~TwoWheelDrive();
+		
+		void driveControll( int leftSpeed, int rightSpeed );
+
+	private:
+		TwoWheelDrive( const TwoWheelDrive &c );
+		TwoWheelDrive& operator=( const TwoWheelDrive &c );
+
+}; 
+
+#endif 
 
