8e06e09de02b65e41bd1cf8ccd072ab5b93e67a6
Woocheck <woocheck@op.pl>
2019-07-13 21:34:29 +0200

    pins numbers

---
+++ pin_settings.h
@@ -0,0 +1,35 @@
+/*
+ * pin_settings.h
+ *
+ * Created: 17.03.2019 21:34:45
+ *  Author: Woocheck
+*/
+
+#ifndef PIN_SETTINGS_H_
+#define PIN_SETTINGS_H_
+
+
+ 
+ #define PIN_LEFT_A 2
+ #define PIN_LEFT_B 3
+ #define PIN_LEFT_ENABLE 21
+ 
+ #define PIN_RIGHT_A 23
+ #define PIN_RIGHT_B 22
+ #define PIN_RIGHT_ENABLE 25
+
+ #define PIN_ENCODER_LEFT_A 0
+ #define PIN_ENCODER_LEFT_B 9
+
+ #define PIN_ENCODER_RIGHT_A 1
+ #define PIN_ENCODER_RIGHT_B 4
+
+ #define PIN_SENSOR_1 6 
+ #define PIN_SENSOR_2 26
+ #define PIN_SENSOR_3 27
+ #define PIN_SENSOR_4 28
+ #define PIN_SENSOR_5 29
+
+ #define PIN_BUTTON 24
+ 
+#endif /* PIN_SETTINGS_H_ */
