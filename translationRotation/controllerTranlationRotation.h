b33d71100a8e989957222137fcd9f4b6e7898698
Woocheck <woocheck@op.pl>
2019-07-19 19:21:33 +0200

    Translation rotation class completed.

---
+++ translationRotation/controllerTranlationRotation.h
@@ -0,0 +1,51 @@
+/* 
+* routeControll.h
+*
+* Created: 15.07.2019 21:50:45
+* Author: Woocheck
+*/
+
+
+#ifndef __TWO_WHEEL_DRIVE_CLASS_H__
+#define __TWO_WHEEL_DRIVE_CLASS_H__
+
+enum class VehicleState { ride, stop, braking };
+
+struct Parameters 
+{
+    VehicleState state = VehicleState::ride;
+    float targetSpeed { 0 };
+    float nextSpeed { 5 };
+    float currentSpeed { 0 };
+    float acceleration { 0 };
+    int roadToPass { 0 };
+    int targetDistance { 0 };
+
+    Parameters( float tSp, float acc, float tDist ): 
+                targetSpeed( tSp ),
+                acceleration( acc ), 
+                targetDistance( tDist) {};
+};
+
+class RouteController
+{
+    private:
+        Parameters _translator;
+        Parameters _rotator;
+    
+    public:
+        RouteController(float nominalSpeed, float acceleration, float distance, float angle  ):
+             _translator( nominalSpeed, acceleration, distance ),
+             _rotator( nominalSpeed, acceleration, angle ) {};
+        int getTranslation();
+        int getRotation();
+    // private:
+        void calculateTranslation();
+        void calculateRotation();
+        bool needToBrake();
+        bool needToRotationBrake();
+        bool reachedDestination();
+        bool reachedAngle();
+};
+
+#endif
---
+++ translationRotation/controllerTranslationRotation.cpp
@@ -0,0 +1,121 @@
+
+/* 
+* routeControll.h
+*
+* Created: 15.07.2019 21:50:45
+* Author: Woocheck
+*/
+
+#include <algorithm>
+#include <cstdlib>
+
+#include "./controllerTranlationRotation.h"
+
+void RouteController::calculateTranslation()
+{
+    if( _translator.state == VehicleState::ride && needToBrake() )
+    {
+        _translator.state = VehicleState::braking;
+        _translator.targetSpeed = _translator.nextSpeed;
+    }
+
+    if( reachedDestination() )
+    {
+       _translator.state = VehicleState::stop;
+       _translator.targetSpeed = _translator.nextSpeed; 
+    }
+
+    if( _translator.currentSpeed < _translator.targetSpeed )
+    {
+        _translator.currentSpeed += _translator.acceleration;
+        if( _translator.currentSpeed > _translator.targetSpeed )
+        {
+            _translator.currentSpeed = _translator.targetSpeed;
+        }
+    }
+
+    if( _translator.currentSpeed > _translator.targetSpeed )
+    {
+        _translator.currentSpeed -= _translator.acceleration;
+        if( _translator.currentSpeed < _translator.targetSpeed )
+        {
+            _translator.currentSpeed = _translator.targetSpeed;
+        }
+    }
+    _translator.roadToPass += _translator.currentSpeed;
+    
+};
+
+void RouteController::calculateRotation()
+{
+    if( _rotator.state == VehicleState::ride && needToRotationBrake() )
+    {
+         _rotator.state = VehicleState::braking;
+        _rotator.targetSpeed = _rotator.nextSpeed;
+    }
+
+    if( reachedAngle() )
+    {
+        _rotator.state = VehicleState::stop;
+        _rotator.targetSpeed = _rotator.nextSpeed;
+    }
+
+    if( _rotator.currentSpeed < _rotator.targetSpeed )
+    {
+        _rotator.currentSpeed += _rotator.acceleration;
+        if( _rotator.currentSpeed > _rotator.targetSpeed )
+        {
+            _rotator.currentSpeed = _rotator.targetSpeed;
+        }
+    }
+
+    if( _rotator.currentSpeed > _rotator.targetSpeed )
+    {
+        _rotator.currentSpeed -= _rotator.acceleration;
+        if( _rotator.currentSpeed < _rotator.targetSpeed )
+        {
+            _rotator.currentSpeed = _rotator.targetSpeed;
+        }
+    }
+
+    if( _rotator.targetDistance > 0) 
+        _rotator.roadToPass += _rotator.currentSpeed; 
+    else 
+        _rotator.roadToPass -= _rotator.currentSpeed;
+}; 
+
+int RouteController::getTranslation()
+{
+    return static_cast<int>( _translator.currentSpeed ) ;
+};
+
+int RouteController::getRotation()
+{
+    return static_cast<int>( _rotator.currentSpeed );
+};
+
+bool RouteController::needToBrake()
+{
+    return ((_translator.currentSpeed - _translator.nextSpeed) *
+            (_translator.currentSpeed - _translator.nextSpeed) /(2*_translator.acceleration)) 
+            >= (_translator.targetDistance - _translator.roadToPass);
+
+};
+
+bool RouteController::needToRotationBrake()
+{
+    return ((_rotator.currentSpeed - _rotator.nextSpeed) *
+            (_rotator.currentSpeed - _rotator.nextSpeed) /(2*_rotator.acceleration)) 
+            >= (std::abs(_rotator.targetDistance) - std::abs(_rotator.roadToPass));
+};
+
+bool RouteController::reachedDestination()
+{
+
+}
+
+bool RouteController::reachedAngle()
+{
+
+}
+     
